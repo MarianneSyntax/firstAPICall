f7d6aff36aca7122a0ac7771e4b2c2469e1413c1
MarianneSyntax <113111255+MarianneSyntax@users.noreply.github.com>
2022-12-07 21:42:48 +0000

    SongAdapter anpassen: dataset ist leere liste, nicht mehr Fake liste aus dem repo

--- app/src/main/java/com/example/firstapicall/adapter/SongAdapter.kt
+++ app/src/main/java/com/example/firstapicall/adapter/SongAdapter.kt
@@ -3,6 +3,7 @@ package com.example.firstapicall.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
@@ -11,15 +12,15 @@ import com.example.firstapicall.data.Repository
 import com.example.firstapicall.data.model.Song
 
 class SongAdapter() : RecyclerView.Adapter<SongAdapter.ItemViewHolder>() {
-    private var repo = Repository()
 
-    private var dataset = repo.loadSongs()
+    private var dataset = listOf<Song>()
 
 
     inner class ItemViewHolder(view: View): RecyclerView.ViewHolder(view){
         val name: TextView = view.findViewById(R.id.song_name)
         val artist: TextView = view.findViewById(R.id.song_artist)
         val songCard: CardView = view.findViewById(R.id.song_card)
+        val image: ImageView = view.findViewById(R.id.song_img)
 
     }
 
