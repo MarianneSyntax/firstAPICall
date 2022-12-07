28eb15e6f5045de9e38867dc25ff57418139be9e
MarianneSyntax <113111255+MarianneSyntax@users.noreply.github.com>
2022-12-07 21:44:24 +0000

    search() funktion im MainViewModel: launcht eine Coroutine, aus der heraus loadSongs im Repository aufgerufen wird (und aus loadSongs im repository wird getResponse in unserer SearchApi vom Typ SongApiService aufgerufen)

--- app/src/main/java/com/example/firstapicall/ui/main/MainViewModel.kt
+++ app/src/main/java/com/example/firstapicall/ui/main/MainViewModel.kt
@@ -1,7 +1,21 @@
 package com.example.firstapicall.ui.main
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.firstapicall.data.Repository
+import kotlinx.coroutines.launch
 
 class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    private val repository = Repository()
+
+    val songs = repository.songs
+
+    fun search(term: String){
+        // hier müssen wir eine Coroutine launchen, weil loadSongs (und das darin aufgerufene getResponse) beides suspend-functions sind,
+        viewModelScope.launch {
+            repository.loadSongs(term)
+        }
+
+    }
 }
