9f0c84659e1b445c7e9df35a2cf4a8853a92f197
MarianneSyntax <113111255+MarianneSyntax@users.noreply.github.com>
2022-12-08 12:46:00 +0000

    nur noch von der id abhändig titel, artist und bild laden:
    man sucht in der songs liste den song mit derselben trackId und setzt mit binding die Elemente im layout auf die Eigenschaften des Songs.
    
    Fertig! Jetzt nutzen wir Coil statt Picasso und geben nur die id als argument im nav graph mit, statt jedes attribut einzeln

--- app/src/main/java/com/example/firstapicall/ui/main/SongDetailFragment.kt
+++ app/src/main/java/com/example/firstapicall/ui/main/SongDetailFragment.kt
@@ -1,20 +1,19 @@
 package com.example.firstapicall.ui.main
 
 import android.os.Bundle
+import coil.load
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
-import com.example.firstapicall.R
-import com.example.firstapicall.databinding.FragmentMainBinding
 import com.example.firstapicall.databinding.FragmentSongDetailBinding
-import com.squareup.picasso.Picasso
 
 
 class SongDetailFragment : Fragment() {
-    private lateinit var viewModel: MainViewModel
+    private val viewModel: MainViewModel by activityViewModels()
     private lateinit var binding: FragmentSongDetailBinding
 
     override fun onCreateView(
@@ -22,20 +21,25 @@ class SongDetailFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        binding = FragmentSongDetailBinding.inflate(inflater, container, false)
+        binding = FragmentSongDetailBinding.inflate(inflater)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
 
-        val title = requireArguments().getString("title")
-        val artist = requireArguments().getString("artist")
-        val image = requireArguments().getString("image")
+        val trackId = requireArguments().getInt("trackId")
 
-        binding.songDetailTitle.text = title
-        binding.songDetailArtist.text = artist
-        Picasso.get().load(image).into(binding.songDetailImg)
+        viewModel.songs.observe(viewLifecycleOwner){ songs ->
+            val song = songs.find { it.trackId == trackId }
+
+            if (song != null){
+                binding.songDetailArtist.text = song.artist
+                binding.songDetailTitle.text = song.track
+                binding.songDetailImg.load(song.artResource)
+            }
+
+        }
+
 
         binding.downloadBtn.setOnClickListener {
             Toast.makeText(requireContext(), "Downloading...", Toast.LENGTH_LONG).show()
